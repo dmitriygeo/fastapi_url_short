b6b725bdcca400e5066bb969818956fd78dfab20
dmitriygeo <103333082+dmitriygeo@users.noreply.github.com>
2025-03-31 02:32:36 +0300

    Update auth_secuirity.py

--- app/auth/auth_secuirity.py
+++ app/auth/auth_secuirity.py
@@ -8,11 +8,11 @@ from sqlalchemy import select
 from app.models.models import User
 from config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 
-# Создаем контекст для хеширования паролей
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token", auto_error=False)
 
-
+"""Аутентификация пользователя"""
 async def authenticate_user(db: AsyncSession, email: str, password: str) -> Optional[User]:
     result = await db.execute(select(User).where(User.email == email))
     user = result.scalar_one_or_none()
@@ -23,16 +23,16 @@ async def authenticate_user(db: AsyncSession, email: str, password: str) -> Opti
         return None
     return user
 
+"""Проверяет соответствие пароля его хешу"""
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    """Проверяет соответствие пароля его хешу"""
     return pwd_context.verify(plain_password, hashed_password)
-
+    
+"""Создание хеш пароля"""
 def get_password_hash(password: str) -> str:
-    """Создает хеш пароля"""
     return pwd_context.hash(password)
-
+    
+"""Создание JWT токен"""
 def create_access_token(data: dict) -> str:
-    """Создает JWT токен"""
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
