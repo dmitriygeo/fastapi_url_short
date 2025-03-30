ebe343122870885214c43809600c5fdcad245468
dmitriygeo <103333082+dmitriygeo@users.noreply.github.com>
2025-03-31 02:35:05 +0300

    Update redis.py

--- app/redis/redis.py
+++ app/redis/redis.py
@@ -1,13 +1,5 @@
-#from redis import asyncio as aioredis
-from config import REDIS_HOST, REDIS_PORT, REDIS_DB
-
-
-# redis = aioredis.from_url(REDIS_URL, encoding="utf8", decode_responses=True)
-#
-# async def get_redis():
-#     return redis
-
 import redis.asyncio as aioredis
+from config import REDIS_HOST, REDIS_PORT, REDIS_DB
 
 def get_redis():
     return aioredis.Redis(
